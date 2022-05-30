46c9f5bcc3848307982e398656d2a3931ce0338f
JESUS ARMANDO ARMANDO <>
2022-05-30 19:35:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function saludo(){
+  console.log('entro aqui')
+  const nombre = document.getElementById('nombre').value;
+
+  const saludo = document.getElementById('saludo').style.visibility.valueOf='none';
+}
